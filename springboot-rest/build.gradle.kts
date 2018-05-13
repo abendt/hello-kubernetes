41ff6b22a53ab8f911273693443d436247b22a20
Alphonse Bendt <alphonse.bendt@gmail.com>
2018-05-13 17:03:47 +0200

    use dockerPreparePlugin to prepare docker image

--- springboot-rest/build.gradle.kts
+++ springboot-rest/build.gradle.kts
@@ -5,6 +5,7 @@ import com.bmuschko.gradle.docker.tasks.container.DockerStopContainer
 import com.bmuschko.gradle.docker.tasks.container.extras.DockerWaitHealthyContainer
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile
+import com.garyclayburg.docker.DockerPreparePluginExt
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 buildscript {
@@ -21,6 +22,9 @@ plugins {
     id("io.spring.dependency-management") version "1.0.5.RELEASE"
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("com.bmuschko.docker-remote-api") version "3.2.8"
+
+    id("com.garyclayburg.dockerprepare") version "1.3.1"
+
 }
 
 repositories {
@@ -44,6 +48,11 @@ dependencies {
     testCompile("io.rest-assured:rest-assured:3.1.0")
 }
 
+configure<DockerPreparePluginExt> {
+    setCommonService("org.springframework.boot:spring-boot-starter")
+    dockerSrcDirectory = "src/main/docker"
+}
+
 tasks {
     withType<KotlinCompile> {
         kotlinOptions {
@@ -53,28 +62,10 @@ tasks {
 
     val bootJar by getting(BootJar::class)
 
-    val unpackFatJar by creating(Copy::class) {
-        dependsOn(bootJar)
-        val dockerWorkingDir = file("$buildDir/docker")
-
-        val archivePath = bootJar.archivePath
-
-        from(zipTree(archivePath))
-
-        into(dockerWorkingDir)
-        includeEmptyDirs = false
-
-        // hack to preserve modification date - required for Docker cache
-        val copyDetails = mutableListOf<FileCopyDetails>()
-
-        eachFile { copyDetails.add(this) }
-        doLast { restoreLastModifiedDate(copyDetails, dockerWorkingDir) }
-    }
-
     val dockerBuildImage by creating(DockerBuildImage::class) {
-        dependsOn(unpackFatJar)
+        remove = true
         tag = "hellorest"
-        inputDir = projectDir
+        inputDir = file("$buildDir/docker")
     }
 
     val dockerCreateContainer by creating(DockerCreateContainer::class) {
